c4b418229f778cbb8e168a859dc740979753cca3
Sanskar Tiwari <sanskar.mk2@gmail.com>
2022-06-15 19:33:17 +0530

    Fresh look

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
                 stb1: "#005f8c",
                 stb2: "#00b9be",
                 stb3: "#9ff4e5",
+                // https://studiodasol.com/11079652
+                Sunglow: "#FBC929",
+                Green_Pigment: "#219C4D",
+                Spanish_Blue: "#286DB6",
+                Cinnabar: "#EA473A",
             },
             screens: {
                 xsm: "521px",
